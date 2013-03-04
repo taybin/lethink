bdee27b3ea5ca8105a150b7d220438f14713cf22
Taybin Rutkin <trutkin@utest.com>
2013-03-03 21:12:09 -0500

    initial commit

---
+++ src/lethink_app.erl
@@ -0,0 +1,16 @@
+-module(lethink_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    lethink_sup:start_link().
+
+stop(_State) ->
+    ok.
