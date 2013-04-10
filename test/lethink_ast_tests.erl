87e10e25ac33d291e0d0e0002157311a829c1058
Taybin Rutkin <trutkin@utest.com>
2013-04-09 23:39:12 -0400

    use ?assertMatch() in eunit tests

--- test/lethink_ast_tests.erl
+++ test/lethink_ast_tests.erl
@@ -5,12 +5,12 @@
 -include("ql2_pb.hrl").
 
 db_test() ->
-    #term{} = lethink_ast:db(<<"test">>, []),
-    {error, _} = lethink_ast:db("test", []),
-    {error, _} = lethink_ast:db(<<"test">>, #term{}).
+    ?assertMatch(#term{}, lethink_ast:db(<<"test">>, [])),
+    ?assertMatch({error, _}, lethink_ast:db("test", [])),
+    ?assertMatch({error, _}, lethink_ast:db(<<"test">>, #term{})).
 
 table_test() ->
-    #term{} = lethink_ast:table(<<"table">>, []),
-    #term{} = lethink_ast:table(<<"table">>, #term{ type = 'DB' }),
-    {error, _} = lethink_ast:table("table", []),
-    {error, _} = lethink_ast:table(<<"table">>, #term{}).
+    ?assertMatch(#term{}, lethink_ast:table(<<"table">>, [])),
+    ?assertMatch(#term{}, lethink_ast:table(<<"table">>, #term{ type = 'DB' })),
+    ?assertMatch({error, _}, lethink_ast:table("table", [])),
+    ?assertMatch({error, _}, lethink_ast:table(<<"table">>, #term{})).
